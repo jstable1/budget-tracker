f1ae5453bec92341f007ea764cbd55dca4816b4b
jstable1 <jessica.stabler@gmail.com>
2022-05-03 10:40:33 -0600

    cleaned up server.js

--- server.js
+++ server.js
@@ -16,13 +16,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// mongoose.connect(MONGODB_URI, {
-//   useNewUrlParser: true,
-//   useFindAndModify: false
-// });
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/budget-tracker', {
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
+  useFindAndModify: false,
   useUnifiedTopology: true
 });
 
