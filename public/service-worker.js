d2539b4dae33075198403454a36f50fa6d12c5bc
jstable1 <jessica.stabler@gmail.com>
2022-05-06 20:18:09 -0600

    updated service worker to get fetch to work

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,16 +1,19 @@
 const APP_PREFIX = 'BudgetTracker-';     
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
+const DATA_CACHE_NAME = 'data-cache-' + VERSION;
 
 const FILES_TO_CACHE = [
   '/index.html',
   '/css/styles.css',
-  '/js/index.js'
+  '/js/index.js',
+  '/js/idb.js',
+  '/manifest.json'
 ];
 
 // Install the service worker
-self.addEventListener('install', function(evt) {
-  evt.waitUntil(
+self.addEventListener('install', function(event) {
+  event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       console.log('Your files were pre-cached successfully!');
       return cache.addAll(FILES_TO_CACHE);
@@ -20,8 +23,8 @@ self.addEventListener('install', function(evt) {
   self.skipWaiting();
 });
 
-self.addEventListener('activate', function(evt) {
-  evt.waitUntil(
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
     caches.keys().then(function(keyList) {
       let cacheKeeplist = keyList.filter(function(key) {
         return key.indexOf(APP_PREFIX);
@@ -40,17 +43,42 @@ self.addEventListener('activate', function(evt) {
   );
 });
 
-self.addEventListener('fetch', function (evt) {
-  console.log('fetch request : ' + evt.request.url)
-  evt.respondWith(
-      caches.match(evt.request).then(function (request) {
-          if (request) {
-            console.log('responding with cache : ' + evt.request.url)
-            return request
-          } else {
-              console.log('file is not cached, fetching : ' + evt.request.url)
-              return fetch(evt.request)
-          }
+self.addEventListener('fetch', function(event) {
+  if (event.request.url.includes('/api/')) {
+    event.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then(cache => {
+          return fetch(event.request)
+            .then(response => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(event.request.url, response.clone());
+              }
+
+              return response;
+            })
+            .catch(err => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(event.request);
+            });
         })
-  )
-})
+        .catch(err => console.log(err))
+    );
+
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get('accept').includes('text/html')) {
+          // return the cached home page for all requests for html pages
+          return caches.match('/');
+        } console.log(fetch)
+      });
+    })
+  );
+});
